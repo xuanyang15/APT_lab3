d4b2cf988fd89f216cc62097ea202683da527bd5
xuanyang15 <yangxuanemail@gmail.com>
2015-11-13 19:28:09 -0600

    change back to right max and coverage 100%

--- project1/Experiment.py
+++ project1/Experiment.py
@@ -1,11 +1,11 @@
 import sys
 
 def largest(list):
-  if len(list) == 0:
-    raise ValueError("Cannot call largest on empty list")
+  #if len(list) == 0:
+    #raise ValueError("Cannot call largest on empty list")
   #max = -sys.maxint # "smallest" possible int
-  max = 0
-  #max = list[0]
+  #max = 0
+  max = list[0]
   for index in range(len(list)):
     if (list[index] > max):
       max = list[index]
