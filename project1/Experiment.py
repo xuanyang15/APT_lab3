52b82321564ab6e899801a1aca02681d5adb47e3
xuanyang15 <yangxuanemail@gmail.com>
2015-11-13 20:23:11 -0600

    change back to correct

--- project1/Experiment.py
+++ project1/Experiment.py
@@ -4,8 +4,8 @@ def largest(list):
   #if len(list) == 0:
     #raise ValueError("Cannot call largest on empty list")
   #max = -sys.maxint # "smallest" possible int
-  max = 0
-  #max = list[0]
+  #max = 0
+  max = list[0]
   for index in range(len(list)):
     if (list[index] > max):
       max = list[index]
